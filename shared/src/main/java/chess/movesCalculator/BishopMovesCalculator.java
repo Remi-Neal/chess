437e8035266844e570785e577226019087ec3f81
Remi <90064274+Remi-Neal@users.noreply.github.com>
2025-01-20 13:44:30 -0700

    Refactored BishopMovesCalculator and PieceMovesCalculator by lifting tryAddMove, isOutOfBounds, and isOccupied methods to PieceMovesCalculator to reuse the code in other child classes. Refactored PieceMovesCalculator to be an abstract class and the methods addMove and pieceMoves to be abstract as well.

--- shared/src/main/java/chess/movesCalculator/BishopMovesCalculator.java
+++ shared/src/main/java/chess/movesCalculator/BishopMovesCalculator.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 import static chess.ChessPiece.PieceType.BISHOP;
 
-public class BishopMovesCalculator implements PieceMovesCalculator {
+public class BishopMovesCalculator extends PieceMovesCalculator {
     private List<ChessMove> moves;
 
     public BishopMovesCalculator() {
@@ -23,57 +23,6 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
     @Override
     public void addMove(ChessMove move) {
         this.moves.add(move);
-    }
-
-    /**
-     * A wrapper class for addMove to validate if a move is valid.
-     * @param position The current position the bishop piece.
-     * @param newPosition The new position we wish to validate.
-     * @return A boolean value. True to continue testing positions and false if an obstruction is met.
-     */
-    @Override
-    public Boolean tryAddMove(ChessBoard board, ChessGame.TeamColor color, ChessPosition position, ChessPosition newPosition) {
-        if(isOutOfBounds(newPosition)) return false;
-        PositionStatus status = isOccupied(board,  color, newPosition);
-        return switch (status) {
-            case PositionStatus.OPEN -> {
-                addMove(new ChessMove(position, newPosition, null));
-                yield true;
-            }
-            case PositionStatus.CAPTURABLE -> {
-                addMove(new ChessMove(position, newPosition, null));
-                yield false;
-            }
-            case PositionStatus.BLOCKED -> false;
-            default ->
-                    throw new RuntimeException("Unknown position status. Expected: 'OPEN', 'BLOCKED', 'CAPTURABLE', or 'INECCESSIBLE'.");
-        };
-        // TODO: add capture functionality canCapture(board, color, ChessPosition
-        // TODO: add same color blocking to parent class isBlocked(board, color, ChessPosition)
-    }
-
-    /**
-     * A method to calculate if a given position is out of bounds.
-     * @param position The position to check.
-     * @return A boolean value. False if the piece is within bounds and true if it is out of bounds.
-     */
-    @Override
-    public Boolean isOutOfBounds(ChessPosition position) {
-        return position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8;
-    }
-
-    /**
-     * A method to calculate if a given position is blocked. Adds blocked position to a list of blocked positions
-     * @param board The current game board.
-     * @param color The current bishop's color.
-     * @param position The position to check.
-     * @return An enum Position status: OPEN, BLOCKED, CAPTURABLE
-     */
-    @Override
-    public PositionStatus isOccupied(ChessBoard board, ChessGame.TeamColor color, ChessPosition position) {
-        if(board.getPiece(position) == null ) return PositionStatus.OPEN;
-        if(board.getPiece(position).getTeamColor() == color) { return PositionStatus.BLOCKED; }
-        return PositionStatus.CAPTURABLE;
     }
 
     @Override
--- shared/src/main/java/chess/movesCalculator/PieceMovesCalculator.java
+++ shared/src/main/java/chess/movesCalculator/PieceMovesCalculator.java
@@ -10,25 +10,46 @@ import java.util.Collection;
 /**
  * Interface for piece specific calculators
  */
-public interface PieceMovesCalculator {
+abstract public class PieceMovesCalculator {
     enum PositionStatus{
         OPEN,
         BLOCKED,
         CAPTURABLE,
         INECCESSIBLE,
     }
-    default void addMove(ChessMove move) {}
-    default Boolean tryAddMove(ChessBoard board, ChessGame.TeamColor color, ChessPosition position, ChessPosition newPosition) { return null; }
-    default Boolean isOutOfBounds(ChessPosition position){ return null; }
-    default PositionStatus isOccupied(ChessBoard board, ChessGame.TeamColor color, ChessPosition position) { return null; }
+
+    public Boolean tryAddMove(ChessBoard board, ChessGame.TeamColor color, ChessPosition position, ChessPosition newPosition) {
+        if(isOutOfBounds(newPosition)) return false;
+        PositionStatus status = isOccupied(board,  color, newPosition);
+        return switch (status) {
+            case PositionStatus.OPEN -> {
+                addMove(new ChessMove(position, newPosition, null));
+                yield true;
+            }
+            case PositionStatus.CAPTURABLE -> {
+                addMove(new ChessMove(position, newPosition, null));
+                yield false;
+            }
+            case PositionStatus.BLOCKED -> false;
+            default ->
+                    throw new RuntimeException("Unknown position status. Expected: 'OPEN', 'BLOCKED', 'CAPTURABLE', or 'INECCESSIBLE'.");
+        };
+    }
+    public Boolean isOutOfBounds(ChessPosition position) {
+        return position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8;
+    }
+    public PositionStatus isOccupied(ChessBoard board, ChessGame.TeamColor color, ChessPosition position) {
+        if(board.getPiece(position) == null ) return PositionStatus.OPEN;
+        if(board.getPiece(position).getTeamColor() == color) { return PositionStatus.BLOCKED; }
+        return PositionStatus.CAPTURABLE;
+    }
+
+    abstract void addMove(ChessMove move);
     /**
      * The calculator for all possible moves a given piece can make
      * @param board Current state of the game board.
      * @param position THe position of the piece we wish to calculate valid moves for.
      * @return A collection of all possible moves that piece can make
      */
-    default Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return null;
-    }
-
+    abstract Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
 }
