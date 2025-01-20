63eb6cbf54b2ccf65e2bd379c13ac7cc7380a2a9
Remi <90064274+Remi-Neal@users.noreply.github.com>
2025-01-20 13:55:01 -0700

    Added documentation to PieceMovesCalculator

--- shared/src/main/java/chess/movesCalculator/PieceMovesCalculator.java
+++ shared/src/main/java/chess/movesCalculator/PieceMovesCalculator.java
@@ -18,6 +18,17 @@ abstract public class PieceMovesCalculator {
         INECCESSIBLE,
     }
 
+    /**
+     * Wrapper method for addMoves. Checks if a given space is INECCESSIBLE, BLOCKED, CAPTURABLE, or OPEN.
+     * This method is called by pieceMove which generates new moves for the method to test agnostic to the current piece.
+     * The pieceMove method interprets the boolean value returned from tryAddMove
+     *
+     * @param board The current board layout.
+     * @param color The current piece's color.
+     * @param position The current position of the piece.
+     * @param newPosition The new position to validate.
+     * @return Boolean if more spaces can be tested.
+     */
     public Boolean tryAddMove(ChessBoard board, ChessGame.TeamColor color, ChessPosition position, ChessPosition newPosition) {
         if(isOutOfBounds(newPosition)) return false;
         PositionStatus status = isOccupied(board,  color, newPosition);
@@ -35,6 +46,12 @@ abstract public class PieceMovesCalculator {
                     throw new RuntimeException("Unknown position status. Expected: 'OPEN', 'BLOCKED', 'CAPTURABLE', or 'INECCESSIBLE'.");
         };
     }
+
+    /**
+     * A method for checking if a given move falls outside the edge of the game board.
+     * @param position The position to test.
+     * @return A boolean true if the position is outside the board or false if it is still on the board
+     */
     public Boolean isOutOfBounds(ChessPosition position) {
         return position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8;
     }
@@ -44,6 +61,10 @@ abstract public class PieceMovesCalculator {
         return PositionStatus.CAPTURABLE;
     }
 
+    /**
+     * Abstract method for addMove. addMove is implemented in child classes to add valid moves to a local 'moves' variable.
+     * @param move A ChessMove to add.
+     */
     abstract void addMove(ChessMove move);
     /**
      * The calculator for all possible moves a given piece can make
