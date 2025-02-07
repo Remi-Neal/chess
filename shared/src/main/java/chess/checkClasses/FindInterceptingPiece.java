26b35fa2e578c33a58805f30ccf4761d7061bf5d
Remi <90064274+Remi-Neal@users.noreply.github.com>
2025-02-07 11:19:45 -0700

    Implemented findPath in FindInterceptingPiece

--- shared/src/main/java/chess/checkClasses/FindInterceptingPiece.java
+++ shared/src/main/java/chess/checkClasses/FindInterceptingPiece.java
@@ -11,24 +11,67 @@ public class FindInterceptingPiece {
     public FindInterceptingPiece() {
     }
 
-    public List<ChessPosition> findPath(ChessPosition attPiece, ChessPosition vicPiece){
+    /**
+     * Calculate the path from an attacking piece to a victim piece. Finds the positions in between the attacking piece and the victim piece
+     * @param attPiece Attacking piece
+     * @param vicPiece Victim piece
+     * @return The positions in between the attacking and victim pieces
+     */
+    public List<ChessPosition> findPath(ChessPosition attPiece, ChessPosition vicPiece) {
         List<ChessPosition> path = new ArrayList<>();
         // Straight attack
-        if(attPiece.getRow() == vicPiece.getRow()){
-            int start = Math.min(attPiece.getColumn(), vicPiece.getColumn());
+        if (attPiece.getRow() == vicPiece.getRow()) {
+            int start = Math.min(attPiece.getColumn(), vicPiece.getColumn()) + 1; // Adjust to only calculate positions in between
             int end = Math.max(attPiece.getColumn(), vicPiece.getColumn());
-            while(start < end){
+            while (start < end) {
+                path.add(new ChessPosition(attPiece.getRow(), start));
                 start++;
-                // TODO: Add col path calc
             }
         }
-        if(attPiece.getColumn() == vicPiece.getColumn()){
-            // TODO: Add row path calc
+        if (attPiece.getRow() == vicPiece.getRow()) {
+            int start = Math.min(attPiece.getRow(), vicPiece.getRow()) + 1; // Adjust to only calculate positions in between
+            int end = Math.max(attPiece.getRow(), vicPiece.getRow());
+            while (start < end) {
+                path.add(new ChessPosition(attPiece.getRow(), start));
+                start++;
+            }
         }
         // Diag attack
-        // TODO: Add diag path calc
+        // Forward diag
+        if ((attPiece.getColumn() > vicPiece.getColumn()) == (attPiece.getRow() > vicPiece.getRow())) { //True == True || False == False
+            int startCol = Math.min(attPiece.getColumn(), vicPiece.getColumn()) + 1;
+            int startRow = Math.min(attPiece.getRow(), vicPiece.getRow()) + 1; // Going from the furthest left to right, bottom to top
+            int endCol = Math.max(attPiece.getColumn(), vicPiece.getColumn());
+            int endRow = Math.max(attPiece.getRow(), vicPiece.getRow());
+            while(startCol < endCol){ // Arbitrary could be rows too
+                path.add(new ChessPosition(startRow,startCol));
+                startCol++;
+                startRow++;
+            }
+        }
+        // TODO: Add Back slash diag
+        // Back diag (second > switched to <)
+        if ((attPiece.getColumn() > vicPiece.getColumn()) == (attPiece.getRow() < vicPiece.getRow())) { //True == True || False == False
+            int startCol = Math.min(attPiece.getColumn(), vicPiece.getColumn()) + 1;
+            int startRow = Math.max(attPiece.getRow(), vicPiece.getRow()) - 1; // Going from the furthest left to right, top to bottom
+            int endCol = Math.max(attPiece.getColumn(), vicPiece.getColumn());
+            int endRow = Math.min(attPiece.getRow(), vicPiece.getRow());
+            while(startCol < endCol){ // Arbitrary could be rows too
+                path.add(new ChessPosition(startRow,startCol));
+                startCol++;
+                startRow--;
+            }
+        }
         return path;
     }
+
+    /**
+     * Finds the chess pieces that can intercept or block an attacking piece from its victim.
+     * @param attPiece The position of the attacking piece.
+     * @param vicPiece The position of the victim piece.
+     * @param gameBoard The game board to use
+     * @return A list of the positions that can intercept the attacking piece
+     */
     public List<ChessPosition> findPieces(ChessPosition attPiece, ChessPosition vicPiece, ChessBoard gameBoard) {
         ChessPiece.PieceType attPieceType = gameBoard.getPiece(attPiece).getPieceType();
         if(attPieceType == ChessPiece.PieceType.KING ||
@@ -38,5 +81,4 @@ public class FindInterceptingPiece {
         throw new RuntimeException("findPieces in FindInterseptingPiece.java not implemented");
         // TODO: Implement for loops to find pieces and paths to test if they can block attacking piece
     }
-
 }
