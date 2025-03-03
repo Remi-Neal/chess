2cfbd3d24e4bcb2a7c62d3f8f8040b793ec6a7af
Remi <90064274+Remi-Neal@users.noreply.github.com>
2025-03-02 21:08:47 -0700

    Added to DataBase authData list, gameData list, and userData list. Added getters and setters for all three lists and updaters that updates data in the lists. Added to DataBase an inner class called ClearDataBase to hide the clear data functions.

--- server/src/main/java/database/DataBase.java
+++ server/src/main/java/database/DataBase.java
@@ -1,4 +1,61 @@
 package database;
+import database.datamodels.*;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBase {
+    static List<AuthtokenData> authData;
+    static List<GameData> gameData;
+    static List<UserData> userData;
+
+    public DataBase(){
+        authData = new ArrayList<>();
+        gameData = new ArrayList<>();
+        userData = new ArrayList<>();
+    }
+
+    public void addAuthData(AuthtokenData data){
+        authData.add(data);
+    }
+    public void addGameData(GameData data){
+        gameData.add(data);
+    }
+    public void addUserData(UserData data){
+        userData.add(data);
+    }
+
+    public void updateAuthData(AuthtokenData data){
+        throw new UnsupportedOperationException("DataBase.updateAuthData not implemented");
+    }
+    public void updateGameData(GameData data){
+        throw new UnsupportedOperationException("DataBase.updateGameData not implemented");
+    }
+    public void updateUserData(UserData data){
+        throw new UnsupportedOperationException("DataBase.updateUserData not implemented");
+    }
+
+    public AuthtokenData getAuthData(){
+        throw new UnsupportedOperationException("DataBase.getAuthData not implemented");
+    }
+    public GameData getGameData(){
+        throw new UnsupportedOperationException("DataBase.getAGameData not implemented");
+    }
+    public UserData getUserData(){
+        throw new UnsupportedOperationException("DataBase.getUserData not implemented");
+    }
+
+    public static class ClearDataBase{
+        public void clearAuthData(){
+            authData.clear();
+        }
+        public void clearUserData(){
+            userData.clear();
+        }
+        public void clearGameData(){
+            gameData.clear();
+        }
+    }
 }
+
