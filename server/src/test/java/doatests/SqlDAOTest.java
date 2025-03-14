049b0da9fc85850d7f9d6a8ad91dd7dcbf505aad
Remi <90064274+Remi-Neal@users.noreply.github.com>
2025-03-14 11:38:24 -0600

    Removed SqlDAOTests and replaced it with Intellij generated SqlDAOTest file.

---
+++ server/src/test/java/doatests/SqlDAOTest.java
@@ -0,0 +1,37 @@
+package doatests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqlDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getConnection() {
+    }
+
+    @Test
+    void createTables() {
+    }
+
+    @Test
+    void makeUserDAO() {
+    }
+
+    @Test
+    void makeGameDAO() {
+    }
+
+    @Test
+    void makeAuthDAO() {
+    }
+
+    @Test
+    void makeResetDAO() {
+    }
+}
--- server/src/test/java/doatests/SqlDAOTests.java
+++
@@ -1,5 +0,0 @@
-package doatests;
-
-public class SqlDAOTests {
-
-}
